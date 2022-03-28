9fc9b3b74dc5426bfb04f8846816e4fdd2484528
s3lva-kumar <98340923+s3lva-kumar@users.noreply.github.com>
2022-03-28 20:29:17 +0530

    Add install-git shellscript

---
+++ install-git.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+  git --version &> /dev/null
+  if [[ $? -eq 0 ]]; then
+    git already installed
+
+  elif [[ has_apt_get ]]; then
+    sudo add-apt-repository ppa:git-core/ppa && sudo apt update
+    sudo apt-get install git -y
+
+  elif [[ has_yum ]]; then
+    sudo yum update -y ; sudo yum install git -y
+
+  else
+    fatal "Could not find apt-get, yum. Cannot install git on this OS."
+      exit 1
+  fi
+
